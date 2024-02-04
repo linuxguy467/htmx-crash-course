dc7ba602b33b9a6afc19a19ccbc64de390a54d45
linuxguy467 <me@matthem.codes>
2024-02-04 17:20:23 -0500

    replace test users with jsonplaceholder data

--- server.js
+++ server.js
@@ -12,21 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Handle GET requests to fetch users
-app.get('/users', (req, res) => {
-  const users = [
-    {
-      id: 1,
-      name: 'John Doe',
-    },
-    {
-      id: 2,
-      name: 'Bob Williams',
-    },
-    {
-      id: 3,
-      name: 'Shannon Jackson',
-    },
-  ];
+app.get('/users', async (req, res) => {
+  // const users = [
+  //   {
+  //     id: 1,
+  //     name: 'John Doe',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Bob Williams',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Shannon Jackson',
+  //   },
+  // ];
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
 
   res.send(`
     <h1 class="text-2xl font-bold my-4">Users</h1>
