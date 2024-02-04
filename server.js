25bfa66d78d103cf460874b6eb7fc66688f80264
linuxguy467 <me@matthem.codes>
2024-02-04 17:31:27 -0500

    create loading spinner

--- server.js
+++ server.js
@@ -28,15 +28,17 @@ app.get('/users', async (req, res) => {
   //   },
   // ];
 
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await response.json();
+  setTimeout(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
 
-  res.send(`
-    <h1 class="text-2xl font-bold my-4">Users</h1>
-    <ul>
-      ${users.map((user) => `<li>${user.name}</li>`).join('')}
-    </ul>
-  `);
+    res.send(`
+      <h1 class="text-2xl font-bold my-4">Users</h1>
+      <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join('')}
+      </ul>
+    `);
+  }, 10000);
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
